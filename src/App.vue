9bd4cd41995ec00f17c6131d3efb124adf90eabc
Nazir Khashiev <nxashiev@gmail.com>
2022-05-14 22:45:51 +0300

    added search and filters

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,69 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button @click="showDialog" style="margin: 15px 0"
-      >Создать пост</my-button
-    >
+    <my-input v-model="searchQuery" placeholder="Поиск..." />
+    <div class="app__btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
 
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
-    <post-list :posts="posts" @remove="removePost" />
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else style="font-size: 24px; font-weight: bold">
+      Идет загрузка...
+    </div>
+    <div ref="observer" class="observer"></div>
+    <!-- <div class="page__wrapper">
+      <div
+        class="page"
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        :class="{
+          'current-page': page === pageNumber,
+        }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 import PostList from "./components/PostList.vue";
 import PostForm from "./components/PostForm.vue";
+import MyButton from "./components/UI/MyButton.vue";
+import axios from "axios";
+import MySelect from "./components/UI/MySelect.vue";
 
 export default {
   components: {
     PostList,
     PostForm,
+    MyButton,
+    MySelect,
   },
 
   data() {
     return {
-      posts: [
-        { id: 1, title: "JavaScript", body: "Описание Поста" },
-        { id: 2, title: "JavaScript 2", body: "Описание Поста 2" },
-        { id: 3, title: "JavaScript 3", body: "Описание Поста 3" },
-        { id: 4, title: "JavaScript 4", body: "Описание Поста 4" },
-      ],
+      posts: [],
       dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: "",
+      searchQuery: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержанию" },
+      ],
     };
   },
 
@@ -45,6 +78,94 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+
+    // changePage(pageNumber) {
+    //   this.page = pageNumber;
+    // },
+
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("Ошибка");
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert("Ошибка");
+      }
+    },
+  },
+
+  mounted() {
+    this.fetchPosts();
+    console.log(this.$refs.observer);
+    const options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
+  },
+
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
+    },
+
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+  },
+
+  watch: {
+    // page() {
+    //   this.fetchPosts();
+    // },
   },
 };
 </script>
@@ -58,5 +179,32 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.page__wrapper {
+  display: flex;
+  gap: 3px;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.current-page {
+  border: 2px solid teal;
+}
+
+.observer {
+  /* height: 30px; */
+  background: gray;
 }
 </style>
